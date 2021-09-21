8273f4afe8389e503b2ea6c7eca98ee8e1dbae01
sah2020 <akmaljonsamandarov@gmail.com>
2021-09-21 12:36:24 +0500

    allArgs

--- RestApiTask/src/main/java/com/epam/esm/payload/ExceptionResponse.java
+++ RestApiTask/src/main/java/com/epam/esm/payload/ExceptionResponse.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+
 @Data
 @NoArgsConstructor
 public class ExceptionResponse {
     private String errorMessage;
     private Integer errorCode;
+
+    public ExceptionResponse(String errorMessage, Integer errorCode) {
+        this.errorMessage = errorMessage;
+        this.errorCode = errorCode;
+    }
 }
